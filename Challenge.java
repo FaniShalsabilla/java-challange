34c4d9cbe4454f5d1b23650dcf8ed60b214bf109
FaniShalsabilla <fanisalsabilla20@gmail.com>
2021-10-12 15:19:53 +0700

    first commit

---
+++ Challenge.java
@@ -0,0 +1,20 @@
+public class Challenge {
+    public static void main (String[] args){
+       // int number = 1234;
+       // System.out.println(firstName + number);
+       String firstName = "Fani";
+       String lastName = "Shalsabilla";
+       String age = "21";
+       String address = "jl. kapten rahmad buddin";
+       String phoneNumber = "081222037515";
+       String hobi = "Berenang";
+       String makananKesukaan = "kebab";
+       System.out.println("first name : " + firstName);
+    System.out.println("last name : " + lastName);
+    System.out.println("Age : " + age);
+    System.out.println("address : " + address);
+    System.out.println("phoneNumber : " + phoneNumber);
+    System.out.println("hobi : " + hobi);
+    System.out.println("makanan kesukaan : " + makananKesukaan);
+        }
+}
